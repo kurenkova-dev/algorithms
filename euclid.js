35808641cff64d91bf21868189ca2da80b95be36
Karina Kurenkova <58998102+kurenkova-dev@users.noreply.github.com>
2020-12-19 08:03:44 +0300

    Create euclid.js

---
+++ euclid.js
@@ -0,0 +1,15 @@
+//Алгоритм Евклида по поиску наибольшего общего делителя
+
+//m, n - числа, НОД которых мы ищем 
+
+function Euclid(m, n) {
+    // базовый случай
+    if(m === 0) { return n;}
+    if(n === 0) { return m;}
+    
+    //рекурсивный случай
+    return Euclid(b, a % b);
+  }
+ 
+ console.log(Euclid(9, 6));
+ console.log(Euclid(6, 9));
